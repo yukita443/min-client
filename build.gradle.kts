6728055c9c0fe516dd65c2d315436ca773be5bff
yukita443 <yukita443.ttt@gmail.com>
2024-07-21 20:18:43 +0900

    Set up project

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+group = "com.github.minclient"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "min-client"
+            packageVersion = "0.1.0"
+        }
+    }
+}
